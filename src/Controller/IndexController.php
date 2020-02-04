6c646ce449cd4f7cdf93131516fdde35e4726ca4
Albert Barreto <albert.barreto@outlook.com>
2020-02-04 10:58:15 -0300

    Twig images for

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -15,8 +15,17 @@ class IndexController extends AbstractController
     public function home()
     {
         $year = random_int(0,100);
+
+        $images = [
+            ['url' => 'images/hotel/intro_room.jpg', 'class' => ''],
+            ['url' => 'images/hotel/intro_pool.jpg', 'class' => ''],
+            ['url' => 'images/hotel/intro_dining.jpg', 'class' => ''],
+            ['url' => 'images/hotel/intro_attractions.jpg', 'class' => ''],
+            ['url' => 'images/hotel/intro_wedding.jpg', 'class' => 'hidesm']
+        ];
+
         return $this->render('index.html.twig',
-            ['year' => $year]
+            ['year' => $year, 'images' => $images]
         );
     }
 }
